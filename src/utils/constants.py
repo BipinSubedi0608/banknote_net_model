4866f7ee8216698821bfd5f0587fc62e15bae06f
BipinSubedi0608 <subedibipin0609@gmail.com>
2025-08-08 22:45:30 +0545

    changed project structure and completed data exploration

---
+++ src/utils/constants.py
@@ -0,0 +1,17 @@
+# Path to raw data files
+RAW_FEATHER_DATA_PATH = "../../data/banknote_net.feather"
+RAW_CSV_DATA_PATH = "../../data/banknote_net.csv"
+
+# Directory to save processed data
+PROCESSED_DATA_DIR = "../../saved/processed/"
+
+# Directory to save trained models and scalers
+SAVED_MODELS_DIR = "../../saved/models/"
+
+# Processed split file names
+X_TRAIN_FILE = "X_train.npy"
+Y_TRAIN_FILE = "y_train.npy"
+X_VAL_FILE = "X_val.npy"
+Y_VAL_FILE = "y_val.npy"
+X_TEST_FILE = "X_test.npy"
+Y_TEST_FILE = "y_test.npy"
