1acf4834510869c38a73f0fdc5395107cbedef9f
BipinSubedi0608 <subedibipin0609@gmail.com>
2025-08-12 21:30:47 +0545

    added __init__ file, but not working

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -3,3 +3,6 @@ import torch
 def set_cuda_if_available():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     return device
+
+if __name__ == '__main__':
+    print('Device set successfully')
